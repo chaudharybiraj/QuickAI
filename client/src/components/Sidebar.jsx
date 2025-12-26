e607622501654cb9364cedddefdc90b9e352bb82
chaudharybiraj <birajc260@gmail.com>
2025-12-26 13:02:23 +0545

    first commit

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = ({ sidebar, setSidebar}) => {
+  return (
+    <div className={``}>
+      
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ client/src/pages/Removebackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Removebackground = () => {
+  return (
+    <div>
+      <h1>Removebackground</h1>
+    </div>
+  )
+}
+
+export default Removebackground
